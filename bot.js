a565eec955bfaaad1a4334838e60e5c5e7e25f77
coenbart <53538095+coenbart@users.noreply.github.com>
2019-07-31 23:40:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,10 +17,5 @@ client.on('message', message => {
   	}
 });
 
-const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
